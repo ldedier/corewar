8ac1a223ca6c9a0eaf82142b75cdbaceaa81aed6
Emmanuelle Muckensturm <emuckens@student.42.fr>
2018-12-10 22:54:32 +0100

    fix st cursor

--- vm_dir/srcs/instructions/st.c
+++ vm_dir/srcs/instructions/st.c
@@ -6,7 +6,7 @@
 /*   By: emuckens <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/03 20:22:56 by emuckens          #+#    #+#             */
-/*   Updated: 2018/12/10 22:24:46 by emuckens         ###   ########.fr       */
+/*   Updated: 2018/12/10 22:53:46 by emuckens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,13 +22,18 @@ int		ins_st(t_vm *vm, t_parameter arg[3], int pl)
 {
 	int		index;
 	char	val;
+	int		i;
 
-//	ft_printf("arg first = %#x arg 2nd = %#x\n", arg[FIRST].value, arg[SECOND].value);
 	index = arg[SECOND].value % IDX_MOD;
 	val = getval(vm, &vm->proc[pl], arg[FIRST]);
-//	ft_printf("index = %#x val = %#x\n", index, val); 
-	vm->arena[(vm->proc[pl].pc + index + arg[0].nb_bytes + arg[1].nb_bytes + arg[2].nb_bytes) % MEM_SIZE] = val;
-//	ft_printf("check arena index = %c\n", vm->arena[index]);
-	loadval(vm, &vm->proc[pl], arg[FIRST], val);
+	i = arg[SECOND].nb_bytes;
+	while (i)
+	{
+		vm->arena[(index + i) % MEM_SIZE] = (val & 255); 
+//		ft_printf("val = %d arena = %#x\n", val, vm->arena[vm->proc[pl].pc + index + i]);
+		val = val >> 8;
+		--i;
+	}
+//	loadval(vm, &vm->proc[pl], arg[FIRST], val);
 	return (SUCCESS);
 }
