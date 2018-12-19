81b3c85f39db3bee4aa6bbe62b1b8d3afa4ca334
Camille MAPOULA <cammapou@e2r9p13.42.fr>
2018-12-19 13:58:37 +0100

    modif

--- asm_dir/srcs/ft_parse_comment.c
+++ asm_dir/srcs/ft_parse_comment.c
@@ -1,17 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_parse_comment.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cammapou <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/12/19 13:41:10 by cammapou          #+#    #+#             */
+/*   Updated: 2018/12/19 13:48:11 by cammapou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "asm.h"
 
-int check_comment(char *str, t_env *env)
+int			check_comment(char *str, t_env *env)
 {
 	char *comment;
 
 	comment = NULL;
 	if (str[0] == '.')
 	{
-		if (!(comment = ft_strndup(str,  ft_strlen(COMMENT_CMD_STRING))))
+		if (!(comment = ft_strndup(str, ft_strlen(COMMENT_CMD_STRING))))
 			return (1);
 		if (ft_strcmp(comment, COMMENT_CMD_STRING) != 0)
-			return(ft_log_error_no_line("Lexical error COMMENT", env));
+			return (ft_log_error_no_line("Lexical error COMMENT", env));
 	}
 	else if (str[0] != '.')
 		return (ft_log_error("Lexical error BEFORE COMMENT", 0, env));
@@ -19,16 +30,16 @@ int check_comment(char *str, t_env *env)
 	return (0);
 }
 
-static int check_after_comment(char *tmp, int i, t_env *env)
+static int	check_after_comment(char *tmp, int i, t_env *env)
 {
-	while(ft_strchr(&tmp[++i], '"'))
+	while (ft_strchr(&tmp[++i], '"'))
 		;
 	if (tmp[i - 1] != '"')
 		return (ft_log_error_no_line("Lexical error", env));
 	while (tmp[i] || tmp[i] == ' ' || tmp[i] == '\t')
 	{
 		if (isprint(tmp[i]))
-			return (ft_log_error_no_line("Syntax error at token AFTER COMMENT", env));
+			return (ft_log_error_no_line("Syntax error AFTER COMMENT", env));
 		i++;
 	}
 	if (ft_strlen(env->champ.header.prog_name) > PROG_NAME_LENGTH)
@@ -36,7 +47,7 @@ static int check_after_comment(char *tmp, int i, t_env *env)
 	return (0);
 }
 
-static int get_comment_other_line(t_env *env, int fd, int i)
+static int	get_comment_other_line(t_env *env, int fd, int i)
 {
 	char	*tmp;
 
@@ -44,11 +55,11 @@ static int get_comment_other_line(t_env *env, int fd, int i)
 	while (get_next_line(fd, &tmp))
 	{
 		if (ft_strlen(tmp) + ft_strlen(env->champ.header.comment) > COMMENT_LENGTH)
-			return (ft_log_error_no_line("Syntax error at token COMMENT", env));
+			return (ft_log_error_no_line("Syntax error COMMENT", env));
 		ft_strcat(env->champ.header.comment, tmp);
 		ft_strcat(env->champ.header.comment, "\n");
 		if (ft_strchr(tmp, '"'))
-			break;
+			break ;
 		free(tmp);
 	}
 	if (check_after_comment(tmp, i, env) == 1)
@@ -61,8 +72,7 @@ static int get_comment_other_line(t_env *env, int fd, int i)
 	return (0);
 }
 
-
-static int read_comment_continue(char *line, int i, t_env *env, int fd)
+static int	read_comment_continue(char *line, int i, t_env *env, int fd)
 {
 	if (line[i] == '\0')
 	{
@@ -72,10 +82,10 @@ static int read_comment_continue(char *line, int i, t_env *env, int fd)
 	}
 	line++;
 	i += 1;
-	while(line[i] || line[i] == '\t' || line[i] == ' ')
+	while (line[i] || line[i] == '\t' || line[i] == ' ')
 	{
 		if (isprint(line[i]))
-			return (ft_log_error("Syntax error at token AFTER COMMENT", i + 1, env));
+			return (ft_log_error("Syntax error AFTER COMMENT", i + 1, env));
 		i++;
 	}
 	if (ft_strlen(env->champ.header.comment) > COMMENT_LENGTH)
@@ -85,23 +95,23 @@ static int read_comment_continue(char *line, int i, t_env *env, int fd)
 	return (0);
 }
 
-int read_comment(char *line, t_env *env, int fd, int i)
+int			read_comment(char *line, t_env *env, int fd, int i)
 {
 	int j;
 
 	j = 0;
 	if (env->champ.header.comment[j])
-		return (ft_log_error("Syntax error at token COMMAND_COMMENT", 0, env));
+		return (ft_log_error("Syntax error COMMAND_COMMENT", 0, env));
 	i += ft_strlen(COMMENT_CMD_STRING);
-	while (line[i] == ' ' || line[i] == '\t')
-		i++;
+	while (line[++i] == ' ' || line[i] == '\t')
+		;
 	if (line[i] != '"')
-		return (ft_log_error_no_line("Lexical error", env));
-	while(line[i] && line[i + 1] != '"')
+		return (ft_log_error("Lexical error", i - 1, env));
+	while (line[i] && line[i + 1] != '"')
 		env->champ.header.comment[j++] = line[++i];
 	env->champ.header.comment[j] = '\0';
 	if (line[i + 1] != '"' && line[i] != '\0')
-			return (ft_log_error("Lexical error", i, env));
+		return (ft_log_error("Lexical error", i, env));
 	if (read_comment_continue(line, i, env, fd) == 1)
 		return (1);
 	return (0);
--- asm_dir/srcs/ft_parse_header.c
+++ asm_dir/srcs/ft_parse_header.c
@@ -3,36 +3,32 @@
 /*                                                        :::      ::::::::   */
 /*   ft_parse_header.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ldedier <ldedier@student.42.fr>            +#+  +:+       +#+        */
+/*   By: cammapou <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/11/30 20:04:45 by ldedier           #+#    #+#             */
-/*   Updated: 2018/12/11 15:12:54 by ldedier          ###   ########.fr       */
+/*   Created: 2018/12/19 13:40:58 by cammapou          #+#    #+#             */
+/*   Updated: 2018/12/19 13:41:03 by cammapou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "asm.h"
 
-// j'ai change les char * en char[PROG_NAME] etc (voir struct s_champion)
-
 int	isprint(int c)
 {
 	return (c >= 33 && c <= 126);
 }
-//devrait mettre a jour
-//le parser sur has_comment, has_name
 
 int	ft_parse_line_header(char *str, t_env *env, int i, int fd)
 {
 	if (ft_strstr(str, NAME_CMD_STRING))
 	{
-		if(check_name(str, env))
+		if (check_name(str, env))
 			return (1);
 		if (read_name(str, env, i, 0) == 1)
 			return (1);
 	}
 	else if (ft_strstr(str, COMMENT_CMD_STRING))
 	{
-		if(check_comment(str, env))
+		if (check_comment(str, env))
 			return (1);
 		if (read_comment(str, env, fd, 0) == 1)
 			return (1);
@@ -40,6 +36,6 @@ int	ft_parse_line_header(char *str, t_env *env, int i, int fd)
 	else if (!ft_strcmp(env->champ.header.prog_name, ""))
 		return (ft_log_error_no_line("Lexical error NAME", env));
 	else if (!ft_strcmp(env->champ.header.comment, ""))
-		return (ft_log_error_no_line("Lexical error COMMENT",env));
+		return (ft_log_error_no_line("Lexical error COMMENT", env));
 	return (0);
 }
