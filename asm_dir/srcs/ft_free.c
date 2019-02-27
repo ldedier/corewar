1aad36744ab765c93b5000bccc8d98934fc2f8c8
Camille MAPOULA <cammapou@e2r12p10.42.fr>
2019-02-27 16:44:00 +0100

    modif ft_free car segfault

--- asm_dir/srcs/ft_free.c
+++ asm_dir/srcs/ft_free.c
@@ -53,5 +53,7 @@ void	ft_free_all(t_env *e)
 	if (e->champ.cor_name)
 		free(e->champ.cor_name);
 	ft_lstdel(&(e->champ.labels), &ft_free_label_tlst);
+	if (!e->champ.instructions)
+		ft_lstdel(&(e->champ.instructions), &ft_free_instruction_tlst);
 	//ft_lstdel(&(e->champ.instructions), &ft_free_instruction_tlst);
 }
