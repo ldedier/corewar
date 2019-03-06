93e2d929887374457d901a06b17a8341e470e914
Emmanuelle Muckensturm <emuckens@student.42.fr>
2019-03-06 13:51:57 +0100

    add missing malloc protection

--- vm_dir/srcs/instructions/live.c
+++ vm_dir/srcs/instructions/live.c
@@ -6,7 +6,7 @@
 /*   By: emuckens <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/10 17:10:34 by emuckens          #+#    #+#             */
-/*   Updated: 2019/03/06 13:50:09 by emuckens         ###   ########.fr       */
+/*   Updated: 2019/03/06 13:51:03 by emuckens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,7 +46,7 @@ int				ins_live(t_vm *vm, t_process *proc, t_parameter arg[3])
 	getval_param_dest(vm, proc, &arg[0], 1);
 	proc->live = 1;
 	++vm->issued_live;
-	proc->live_cycle = vm->total_cycle; // enlever, gere par proc->pending_ins.op
+	proc->live_cycle = vm->total_cycle;
 	player = get_player_num(vm, vm->proc, arg[0].value);
 	display_proc_ins(vm, proc);
 	if (player)
@@ -55,7 +55,8 @@ int				ins_live(t_vm *vm, t_process *proc, t_parameter arg[3])
 		player->live++;
 		player->last_live_cycle = vm->total_cycle;
 		vm->winner = player;
-		live = (t_fade *)ft_memalloc(sizeof(t_fade));
+		if (!(live = (t_fade *)ft_memalloc(sizeof(t_fade))))
+			return (FAILURE);
 		live->pc = proc->pc;
 		live->color = player->color.value;
 		live->value = FADE_LEN;
