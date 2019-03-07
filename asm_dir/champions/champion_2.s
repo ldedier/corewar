3d103784848d76a6bb73c8ee9fb4a8818d5f05e3
Camille MAPOULA <cammapou@e2r11p11.42.fr>
2019-03-07 14:08:08 +0100

    modif champ

--- asm_dir/champions/champion_2.s
+++ asm_dir/champions/champion_2.s
@@ -4,23 +4,14 @@
 
 begin:
 lfork %2048
-zjmp    %:init #--> base
-
-live:
-live    %1
-zjmp    %:init
 
 init:
-ld        %57672705, r4
-fork  %:attack
-zjmp    %:live1
-
-live1:
-live    %42
+live    %1
+ld      %57672705, r4
+fork    %:attack
+live    %1
 ld %0, r2
 fork    %:save
-zjmp    %:live
-
 
 attack:
 lfork %2048
@@ -70,9 +61,6 @@ st      r4, 300
 live %1
 st      r4, 300
 zjmp %:attack
-zjmp %:live1
-
-
 sti r1, %:save, %1
 
 save:
